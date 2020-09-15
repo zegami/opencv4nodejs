259884ec19f2ec43a4a6f67730c99eb3fec6fd39
Matthew Hall <matthew.hall@greymethod.org.uk>
2020-09-15 13:40:09 +0000

    Update parseEnv.js

--- install/parseEnv.js
+++ install/parseEnv.js
@@ -1,12 +1,23 @@
+const fs = require('fs');
 const envName = process.argv[2]
 
 if (!envName) {
   throw new Error('no env name passed to parseEnv')
 }
 
+let cache = {};
+
+if (fs.existsSync('envCache.json')) {
+  const cache = fs.openSync('envCache.json');
+  console.log('Found environment cache: ' + cache)
+} else {
+  console.warn('Environment cache could not be found?')
+}
+
 const outputs = (process.env[envName] || '').split('\n')
 
 if (envName == 'OPENCV4NODEJS_INCLUDES' && process.env[envName] === undefined) {
+
   console.log('install','NODE_ENV is '+process.env.NODE_ENV);
   console.warn('OPENCV4NODEJS_INCLUDES is empty. Build may fail when referencing opencv2/* without relevant environment configuration.');
 }
